0590329c103a6c4180442ba18aa816837a4d2f3b
Dhanraj  Sahu <rajofficials23@gmail.com>
2026-05-13 13:53:36 +0530

    fixes bugs

--- src/controllers/user.contrroller.js
+++ src/controllers/user.contrroller.js
@@ -109,7 +109,7 @@ const loginUser = asyncHandler(async(req, res)=>{
 
 const {email, username, password}=req.body
 
-if (!username ||!email ){
+if (!username && !email ){
   throw new ApiError(400, "username and  email  is required ")
 
 }
