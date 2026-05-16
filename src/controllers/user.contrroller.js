d0bd31bbb0c7f7fa8a3239581cf7cf86b0854918
Dhanraj  Sahu <rajofficials23@gmail.com>
2026-05-16 16:33:46 +0530

    Add getUserChannelProfile endpoint and improve response messages

--- src/controllers/user.contrroller.js
+++ src/controllers/user.contrroller.js
@@ -247,7 +247,7 @@ const getCurrentUser = asyncHandler(async(req, res)=>{
       return res
       .status(200)
       .json(200,
-         req.user, "currents ser fetched successfully ")
+         req.user, "current user fetched successfully ")
 
 })
 
@@ -335,8 +335,78 @@ const updateUserCoverImage = asyncHandler(async(req, res)=>{
             new ApiResponse(200, user, " Cover image updated Successfully ")
            )
 
+})
 
+const  getUserChannelProfile = asyncHandler(async(req, res)=>{
+   const {username } =req.params
+   if(!username?.trim()){
+    throw new ApiError(400, " username is required")
+   }
 
+   const channel = await  User.aggregate([
+    {
+      $match: {
+        username: username.toLowerCase()
+      }
+      
+    },
+     {
+        $lookup: {
+          from: "subscription",
+          localField: "_id",
+          foreignField: "channel",
+          as: "subscribers" 
+        }
+      },
+      {
+        $lookup: {
+          from: "subscription",
+          localField: "_id",
+          foreignField: "subscriber",
+          as: "subscribersTo" 
+        }
+      },
+      {
+        $addFields:{
+          subscribersCount: {
+            $size: "$subscribers",
+          },
+          channelsSubscribedToCount: {
+            $size: "$subscribedTo"
+          }, 
+          isSubscribed: {
+            $cond: {
+              if: {
+                $in:[req.user?._id, "$subscribers.subscriber"]
+              },
+              then: true ,
+              else : false 
+
+            }
+          }
+        }
+      },
+      {
+        $project: {
+          fullName:1,
+          username:1,
+          subscribersCount:1,
+          channelsSubscribedToCount:1,
+          isSubscribed:1,
+          avatar:1,
+          coverImage:1,
+          email:1
+          
+        }
+      }
+   ])
+ if(!channel?.length){
+    throw new ApiError(404, " channel does not exists")
+ }
+ return res.status(200)
+           .json(
+            new ApiResponse(200, channel[0], " User Channel profile fetched Successfully ")
+           )
 })
 
 
@@ -350,7 +420,8 @@ export {
   getCurrentUser,
   updateAccountDetails,
   updateUserAvatar,
-  updateUserCoverImage
+  updateUserCoverImage,
+  getUserChannelProfile
 
 } 
 
