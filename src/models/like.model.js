732026340730e5bb515043856ed5918380569f90
Dhanraj  Sahu <rajofficials23@gmail.com>
2026-05-19 18:07:45 +0530

    Add models for comments, likes, playlists, and tweets with schemas and exports

---
+++ src/models/like.model.js
@@ -0,0 +1,27 @@
+  import mongoose  from "mongoose";
+
+
+  const likeSchema = new Schema (
+    {
+      video :{
+              type: Schema.Type.ObjectId,
+              ref:  "Video" 
+        },
+
+         comment :{
+              type: Schema.Type.ObjectId,
+              ref:  "Comment" 
+        },
+         tweet :{
+              type: Schema.Type.ObjectId,
+              ref:  "Tweet" 
+        },
+         likedBy :{
+              type: Schema.Type.ObjectId,
+              ref:  "User" 
+        },
+    } ,
+    {timestamps: true})
+
+    export const  Like =mongoose.model("Like", likeSchema)
+    
